122059258b7b6a2a535812f424a201d58d6ccd31
Ruslan Spivak <ruslan.spivak@gmail.com>
2011-12-24 23:15:28 -0500

    Add license information

--- httpcode.el
+++ httpcode.el
@@ -1,21 +1,30 @@
 ;;; httpcode.el --- explains the meaning of an HTTP status code
-
+;;
 ;; Copyright (C) 2011  Ruslan Spivak
-
+;;
 ;; Author: Ruslan Spivak <ruslan.spivak@gmail.com>
 ;; URL: http://github.com/rspivak/httpcode.el
 ;; Version: 0.1
-;; Keywords: convenience
-
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+;;
 ;;; Commentary:
-
+;;
 ;; Explain the meaning of an HTTP status code. Copy httpcode.el to your
 ;; load-path and add to your .emacs:
-
+;;
 ;;   (require 'httpcode)
-
+;;
 ;; Then run it with with M-x hc
-
+;;
 ;;; Code goes here:
 
 (defconst http-codes
